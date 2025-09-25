9c54edd1bac8538d6cdff4057754b5950e09db37
elkettani <youssef.elkettani@stud.th-luebeck.de>
2025-09-25 17:31:01 +0200

    First Push

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
@@ -21,4 +22,46 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+app.use(express.urlencoded({extended: true}));
+
+let urlDB = {};
+let idCounter = 1;
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+
+  try {
+    new URL(url);
+  } catch {
+    return res.json({ error: "invalid url" });
+  }
+
+
+  dns.lookup(new URL(url).host, (err) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    }
+
+
+    urlDB[idCounter] = url;
+    res.json({
+      original_url: url,
+      short_url: idCounter
+    });
+
+    idCounter++;
+  });
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const short_url = req.params.short_url;
+  const originalUrl = urlDB[short_url];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: "invalid url" });
+  }
 });
